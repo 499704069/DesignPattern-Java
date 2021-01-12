46e0525e446f370ba953090beb13e23c2cc3686f
zhangyao <zhangyao@shubidatech.com>
2021-01-12 10:24:57 +0800

    修改目录结构

--- src/main/java/com/my/entity/User.java
+++ src/main/java/com/my/entity/User.java
@@ -3,13 +3,15 @@ package com.my.entity;
 import lombok.Data;
 
 /**
- * @author zhangyao
+ * @author zy
  * @version 1.0
- * @date 2021/1/12 9:44
+ * @date 2021/1/12 10:23
  */
+
 @Data
 public class User {
 
     private String id;
+
     private String name;
 }
