423995943272bf501472a9edd057a03116ba32ec
zhangyao <zhangyao@shubidatech.com>
2021-01-12 09:46:28 +0800

    首次新建文件

---
+++ designpattern/src/main/java/com/my/entity/User.java
@@ -0,0 +1,21 @@
+package com.my.entity;
+/*
+ * Copyright(c) 2017-2020, 深圳市链融科技股份有限公司
+ * ShenZhen LianRong Technology Co., Ltd.
+ * All rights reserved.
+ */
+
+
+import lombok.Data;
+
+/**
+ * @author zhangyao
+ * @version 1.0
+ * @date 2021/1/12 9:44
+ */
+@Data
+public class User {
+
+    private String id;
+    private String name;
+}
